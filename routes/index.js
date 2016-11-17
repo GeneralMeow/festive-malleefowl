29b63dba5f1c537d2f1d8148bcc65ef9f80c694e
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2016-11-17 10:13:09 -0800

    deployed to heroku. Connection problems occuring

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose')
-const DATABASE_URL =   'mongodb://festive:12345@ds155727.mlab.com:55727/mallee_books'//'mongodb://localhost:27017/mallee_books'
+const DATABASE_URL =   'mongodb://localhost:27017/mallee_books'
 let express = require('express')
 let router = express.Router()
 let { displayBooks } = require('../model/db')
